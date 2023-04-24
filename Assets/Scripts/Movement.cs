89c53c845c9a8301290258b0eae765e150105aa6
Shiro <ericmapo@gmail.com>
2023-04-24 14:12:10 +0200

    fallos arreglados

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,11 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-/*
-Vector2 groundCheckPosition = transform.position + (Vector2.up * -1);
-float groundCheckRadius = 0.5f;
-Gizmos.DrawWireSphere(groundCheckPosition, groundCheckRadius);
-*/
 
 public class Movement : MonoBehaviour
 {
@@ -18,9 +13,6 @@ public class Movement : MonoBehaviour
     public Vector2 velocity;
     public LayerMask floorMask;
     public bool isGrounded;
-    public bool hasJumped = false;
-    private int jumpCount = 2;
-    public int currentJump = 0;
     public GameObject hitObject;
     public Vector2 groundCheckPosition;
     public float groundCheckRadius = 0.05f;
@@ -32,6 +24,8 @@ public class Movement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         _animator = GetComponentInChildren<Animator>();
+
+        //blackOut.FadeOut(); //No funcional por ahora, revisar para la pre-alpha
     }
 
     //Se va a encaragr de leer los inputs y calcular la velocidad
@@ -39,37 +33,17 @@ public class Movement : MonoBehaviour
     {
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        if (!Input.GetButton("Vertical"))
-        {
-            direction.Normalize();
-        }
+        
 
         velocity = new Vector2(direction.x * movementSpeed, rb.velocity.y);
 
-        //if (isGrounded && Input.GetButtonDown("Vertical"))
-        //{
-        //    velocity = new Vector2(direction.x * movementSpeed, jumpSpeed);
-        //}
-
-        if (isGrounded && Input.GetAxisRaw("Vertical") > 0 && !hasJumped)
+        if (isGrounded && Input.GetButton("Jump"))
         {
-            velocity = new Vector2(direction.x * movementSpeed, direction.y * movementSpeed);
-            hasJumped = true;
-            currentJump++;
-
+            velocity = new Vector2(direction.x * movementSpeed, jumpSpeed);
         }
-
-        if (!isGrounded && currentJump < jumpCount && Input.GetKeyDown(KeyCode.W) && hasJumped)
-        {
-            //velocity = new Vector2(direction.x * movementSpeed, direction.y * movementSpeed);
-            rb.AddForce(new Vector2(direction.x * movementSpeed, direction.y * movementSpeed), ForceMode2D.Impulse);
-            hasJumped = true;
-            currentJump++;
-        }
-        
 
         _animator.SetFloat("speed", velocity[0]);
-        //_animator.SetFloat("jump", direction.y * movementSpeed);
+        _animator.SetFloat("jump", direction.y * movementSpeed);
 
         groundCheckPosition = new Vector2(transform.position.x, transform.position.y - 0.6f);
     }
@@ -87,23 +61,21 @@ public class Movement : MonoBehaviour
         {
             hitObject = null;
         }
+
+
         if (hitObject != null)
         {
             isGrounded = true;
-            hasJumped = false;
-            currentJump = 0;
         }
         else
         {
             isGrounded = false;
         }
 
-        if (Input.GetAxis("Horizontal") > 0.1f || Input.GetAxis("Horizontal") < -0.1f || Input.GetAxis("Vertical") > 0.1f)
-            rb.velocity = velocity;
+        rb.velocity = velocity;
 
 
     }
-
     void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
@@ -114,5 +86,4 @@ public class Movement : MonoBehaviour
     {
         transform.position = new Vector2(-6f, -3.5f);
     }
-
 }
