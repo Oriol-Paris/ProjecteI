59dafbd02685660299e7bb2f550ecdf23a36f20f
Èric <eric.maestre@enti.cat>
2023-05-19 10:47:11 +0200

    teleport exists

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+   
+    private Transform destination;
+
+    public bool isTeleport;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isTeleport == true)
+        {
+            destination = GameObject.FindGameObjectWithTag("Portal Destination").GetComponent<Transform>();
+        }
+    }
+
+    
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"{other.name} ha entrado");
+        other.transform.position = new Vector2(destination.position.x, destination.position.y);
+    }
+}
