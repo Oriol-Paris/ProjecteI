c85c53dcabff8aa304fc37c9799cc5c57edef687
Shiro <eric.maestre@enti.cat>
2023-05-27 18:01:20 +0200

    teleport funciona, Oriol contesta de una vez

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Teleport : MonoBehaviour
+
+
+public class Teleporter : MonoBehaviour
 {
-    public Transform destination;
-    public Transform player;
 
-    void (onCollision)
+    Transform player;
+    [SerializeField] Transform destination;
 
-    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        if (destination == null)
+            destination = transform.GetChild(0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player.gameObject)
+        {
+            player.position = destination.position;
+        }
+    }
+
 }
+    
+
