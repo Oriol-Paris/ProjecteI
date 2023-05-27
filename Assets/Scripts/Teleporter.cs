7cc4034b4f9d2ab6d2784afba1f020bda9667bde
Shiro <eric.maestre@enti.cat>
2023-05-27 16:42:00 +0200

    revisando teleporter

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Teleporter : MonoBehaviour
+public class Teleport : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform destination;
+    public Transform player;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void (onCollision)
+
+    
 }
